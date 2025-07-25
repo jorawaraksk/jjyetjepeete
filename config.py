a8b27692dec774c79a3f8193d7ab55ddebc61245
jorawaraksk <jorawarzu@proton.me>
2025-07-25 22:52:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "123456"))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
+API_ID = int(os.getenv("API_ID", "16732227"))
+API_HASH = os.getenv("API_HASH", "8b5594ad7ad37f3a0a7ddbfb3963bb51")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7141984677:AAFztMNvuW-CYbsM1Ydq0zTZ6OQumWSEHTs")
+OWNER_ID = int(os.getenv("OWNER_ID", "5868426717"))
 FFMPEG_CODE = os.getenv("FFMPEG_CODE", "-preset fast -crf 28")
 LOG_FILE = "bot.log"
